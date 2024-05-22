4f929f3a7be97380438e036eb34b1e732bc28940
FriederikeKi <fri_mo@t-online.de>
2024-05-22 17:58:20 +0200

    Do exercises 01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,27 @@
-const repeatString = function() {
+/* PSEUDOCODE
+empty array
+FOR [num times]
+    push string into array 
+FOR END
+join all elements of the array and return
 
+if num negative --> return "ERROR"
+if string = "" --> return ""
+*/
+
+
+const repeatString = function(string, num) {
+    const array = [];
+    for (let i = 0; i < num; i++) {
+        array.push(string);
+    }
+    let result = array.join('');
+
+    if (num < 0) {
+        result = 'ERROR';
+    }
+
+    return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+/* PSEUDOCODE
+split string into an array of every character as one array element
+reverse the order of the array
+join the elements of the array to one string
+*/
 
+const reverseString = function(string) {
+  const arrayOfStringElements = string.split("");
+  const arrayReversed = arrayOfStringElements.reverse();
+  const stringReversed = arrayReversed.join("");
+  return stringReversed;
 };
 
 // Do not edit below this line
